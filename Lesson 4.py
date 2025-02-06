6afdb6e4e33e4b311a97b87885262a07f4b2ef8b
Manaal247 <1060580@pdsb.net>
2025-02-06 12:20:50 -0500

    Create Lesson 4.py

---
+++ Lesson 4.py
@@ -0,0 +1,9 @@
+num = (" 9 ") 
+num = int(9) 
+num2 = num / 6
+print("When", num, " is divided by 6", "the result is:", num2)
+
+num = (" 8 ") 
+num = int(num) 
+num2 = num /(23)
+print("When", num, " is divided by 6", "the result is:", num2)
