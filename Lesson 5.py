a1727887d542685673a06ab03d1c6ef7aa770317
Manaal427 <1060580@pdsb.net>
2025-02-11 13:30:05 -0500

    Update Lesson 5.py

--- Lesson 5.py
+++ Lesson 5.py
@@ -2,3 +2,8 @@ import math
 print ("Area of a norman window calculator")
 radius = input("please input a radius: ")
 radius = float(radius)
+rad = math.pow(radius, 2)
+area = 0.5*math.pi*rad
+area1 = 4*rad
+totalarea = area + area1
+print ("The dimensions of the norman window are", totalarea)
