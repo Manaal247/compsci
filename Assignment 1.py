378d97f7459e80d3656675fcdeba78c315146d9d
Manaal427 <1060580@pdsb.net>
2025-02-24 12:43:14 -0500

    Create Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,4 @@
+print ("Welcome to the even and odd detector!")
+print ("This program determines if the product of two whole positive numbers will be even or odd!")
+number = int(input("Please enter your first number"))
+numberr = int(input("Please enter your second number"))
