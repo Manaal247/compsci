545e75cf6459ec0810fa488b89f5f9585adb8961
Manaal427 <1060580@pdsb.net>
2025-02-25 13:25:49 -0500

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -4,6 +4,6 @@ number = int(input("Please enter your first number"))
 numberr = int(input("Please enter your second number"))
 product = number * numberr
 if product % 2 == 0:
-  print (f"the product of (number) x (numberr) will be an even number")
+  print ("the product of (number) x (numberr) will be an even number")
 else:
-  print (f"The product of (number) x (numberr) will be an odd number")
+  print ("The product of (number) x (numberr) will be an odd number")
