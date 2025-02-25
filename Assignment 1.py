9e8ba03d396ddf545299d11054f2dd0f509ac812
Manaal427 <1060580@pdsb.net>
2025-02-25 13:25:19 -0500

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -2,3 +2,8 @@ print ("Welcome to the even and odd detector!")
 print ("This program determines if the product of two whole positive numbers will be even or odd!")
 number = int(input("Please enter your first number"))
 numberr = int(input("Please enter your second number"))
+product = number * numberr
+if product % 2 == 0:
+  print (f"the product of (number) x (numberr) will be an even number")
+else:
+  print (f"The product of (number) x (numberr) will be an odd number")
