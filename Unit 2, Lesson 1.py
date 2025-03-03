26d65f17532ff5deb0e2aade8111f205480d5008
Manaal427 <1060580@pdsb.net>
2025-03-03 13:37:40 -0500

    Update Unit 2, Lesson 1.py

--- Unit 2, Lesson 1.py
+++ Unit 2, Lesson 1.py
@@ -1,15 +1,15 @@
-score = input("Enter a number between 1 and 100:")
-if score.isdigit():
-  score = int(score)
-  if 80 <= score <= 100:
+x = input("Enter a number between 1 and 100:")
+if x.isdigit():
+  x = int(x)
+  if 80 <= x <= 100:
     print("Grade: A")
-  elif 70 <= score <= 79:
+  elif 70 <= x <= 79:
     print("Grade: B")
-  elif 60 <= score <= 69:
+  elif 60 <= x <= 69:
     print("Grade: C")
-  elif 50 <= score <= 59:
+  elif 50 <= x <= 59:
     print("Grade: D")
-  elif 1 <= score <= 50:
+  elif 1 <= x <= 50:
     print("Grade: F")
   else:
     print("Invalid number. Please enter a number between 1 and 100.")
