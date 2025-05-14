24d3cf4baa8b21e20580ef9192509cdeea68b062
Manaal427 <1060580@pdsb.net>
2025-05-14 13:00:14 -0400

    Delete Assignment 1.2.py

--- Assignment 1.2.py
+++
@@ -1,5 +0,0 @@
-import math
-print ("I will find the cube's inner diagonal for any edge length!")
-edge_length = float(input("Please enter the edge length of your cube: "))
-inner_diagonal = edge_length * math.sqrt(3)
-print (f"The length of the inner diagonal of a cube with side length {edge_length} is: f{inner_diagonal: .2f}")
