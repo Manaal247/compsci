5bea3ae9243f2d5bf63d17a25bf9874eb2afed51
Manaal427 <1060580@pdsb.net>
2025-05-15 08:28:59 -0400

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,26 +1,26 @@
-Author: Manaal Shahid
-Revison date: 05/15/2025
-Program: Wordle 
-Description: A program that searches for a wordle date or wordle word in a wordle database
-Programming Library:
-     dates (list) = list of the wordle dates
-     words (list) = list of the wordle words
-     order (int) = the month in order of the time it occurs
-     months (list) = list of the months
-     date (string) = string format of the date YYYYMMDD
-     line (string) = line of the file
-     p (string) = the day in string format
-     q (string) = the month in string format
-     r (string) = the year in string format
-     file (object) = the file object to open the file
-     word (string) = the entered word
- found_date (string) = the found date of the entered word
-     date (string) = the entered string
-     found_word (string) = the found word of the entered date
-     min_date (int) = the minimum of the found wordle
-     max_date (int) = the maximum of the found word in the wordle
-     d_or_w (string) = asking the user if they find a word or a date
-"""
+#Author: Manaal Shahid
+#Revison date : 5/15/2024
+#Program: Wordle
+#Description: A program that searches for a wordle date or wordle word in the wordle database
+#Programming library:
+    #dates (list) = list of the wordle dates
+    #words (list) = list of the wordle words
+    #order (int) = the month in order of the time it occurs
+    #months (list) = list of the months
+    #date (string) = string format of the date YYYYMMDD
+    #line (string) = line of the file
+    #p (string) = the day in string format
+    #q (string) = the month in string format
+    #r (string) = the year in string format
+    #file (object) = the file object to open the file
+    #word (string) = the entered word
+    #found_date (string) = the found date of the entered word
+    #date (string) = the entered string
+    #found_word (string) = the found word of the entered date
+    #min_date (int) = the minimum of the found wordle
+    #max_date (int) = the maximum of the found word in the wordle
+    #d_or_w (string) = asking the user if they find a word or a date
+
 
 dates = []
 words = []
@@ -32,7 +32,8 @@ def merge(p, q, r):
     for i in range(len(months)):
         if months[i].lower() == q.lower():
             order = i + 1
-             order = f"{order:02d}"
+
+    order = f"{order:02d}"
     p = f"{int(p):02d}"
     date = f"{r}{order}{p}"
     return int(date)
@@ -49,7 +50,7 @@ def scanfile():
             dates.append(date)
             words.append(word)
         file.close()
-         except FileNotFoundError:
+    except FileNotFoundError:
         print("File Error: File Not Found")
 
 def search_by_word(word):
@@ -65,7 +66,8 @@ def search_by_date(date):
             found_word = words[i]
             return date, found_word
     return date, None
-    scanfile()
+
+scanfile()
 min_date = int(merge("19", "Jun", "2021"))
 max_date = int(merge("21", "Apr", "2024"))
 
@@ -84,7 +86,8 @@ if d_or_w.lower() == 'd':
             print(f"{date} is too early. No wordles occurred before {min_date}. Enter a later date.")
         elif int(date) > max_date:
             print(f"{date} is too recent. Our records only go as late as {max_date}. Please enter an earlier date.")
-        else: date, word = search_by_date(date)
+        else:
+            date, word = search_by_date(date)
             if word:
                 print(f"The word entered on {date} was {word}.")
             else:
